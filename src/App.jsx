60735a249019704f810c0db49a040824f66e01f4
Matheus Valenzuela <matheuszvalenz@gmail.com>
2024-01-15 18:49:32 +0000

    Added additional components, almost finished with hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import {  Hero,
+import {
+  Hero,
   Footer,
   CustomerReviews,
   PopularProducts,
   Services,
   SpecialOffer,
   Subscribe,
-  SuperQuality,} from './sections/index'
+  SuperQuality,
+} from "./sections/index";
+import Nav from "./Components/Nav";
 
 const App = () => (
   <main className="relative">
-    <Nav></Nav>
-    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-    <section className="padding"></section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className=" bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="padding bg-black padding-x padding-t pb-8">Footer</section>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className=" bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
